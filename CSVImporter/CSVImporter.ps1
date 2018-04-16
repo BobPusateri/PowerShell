b77a3d1f5a1d0d79caaf576e1b39bb1c7146968b
Bob Pusateri <bob@bobpusateri.com>
2018-04-16 13:53:46 -0500

    Added for April 2018 blog post

---
+++ CSVImporter/CSVImporter.ps1
@@ -0,0 +1,42 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$DateSuffix = (Get-Date).ToString("yyyyMMddHHmmss")
+
+$downloadURI = "https://data.cityofchicago.org/api/views/ygr5-vcbg/rows.csv?accessType=DOWNLOAD"
+$downloadFile = "D:\DownloadTemp\TowedVehicles_$DateSuffix.csv"
+
+# download file
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($downloadURI, $downloadFile)
+
+
+# Load from CSV into staging table
+Import-DbaCsvToSql -Csv $downloadFile -SqlInstance 1InstanceName -Database TowedVehicles -Table TowedVehiclesSTG `
+-Truncate -FirstRowColumns
+
+
+# Move new rows from staging into final table
+Invoke-Sqlcmd2 -ServerInstance InstanceName -Database TowedVehicles `
+-Query "INSERT INTO [dbo].[TowedVehicles]
+SELECT
+	[ID],
+	[TowDate],
+	[Make],
+	[Style],
+	[Model],
+	[Color],
+	[Plate],
+	[State],
+	[TowedToFacility],
+	[FacilityPhone]
+FROM (
+	SELECT
+		s.*,
+		ROW_NUMBER() OVER (PARTITION BY s.ID ORDER BY s.ID) AS n
+	FROM [dbo].[TowedVehiclesStgNative] s
+	LEFT JOIN [dbo].[TowedVehicles] v ON s.ID = v.ID
+	WHERE v.ID IS NULL
+) a
+WHERE a.n = 1"
+
+
---
+++ CSVImporter/CSVImporterSetup.sql
@@ -0,0 +1,33 @@
+﻿USE [TowedVehicles]
+GO
+
+-- CREATE STAGE TABLE
+CREATE TABLE [dbo].[TowedVehiclesSTG](
+	[TowDate] [date] NOT NULL,
+	[Make] [nchar](4) NULL,
+	[Style] [nchar](2) NULL,
+	[Model] [nchar](4) NULL,
+	[Color] [nchar](3) NULL,
+	[Plate] [nchar](8) NULL,
+	[State] [nchar](2) NULL,
+	[TowedToFacility] [nvarchar](75) NULL,
+	[FacilityPhone] [nchar](14) NULL,
+	[ID] [int] NOT NULL
+);
+
+
+-- CREATE FINAL TABLE
+CREATE TABLE [dbo].[TowedVehicles](
+	[ID] [int] NOT NULL,
+	[TowDate] [date] NOT NULL,
+	[Make] [nchar](4) NULL,
+	[Style] [nchar](2) NULL,
+	[Model] [nchar](4) NULL,
+	[Color] [nchar](3) NULL,
+	[Plate] [nchar](8) NULL,
+	[State] [nchar](2) NULL,
+	[TowedToFacility] [nvarchar](75) NULL,
+	[FacilityPhone] [nchar](14) NULL,
+	CONSTRAINT PK_TowedVehicles PRIMARY KEY CLUSTERED (ID)
+); 
+
